fc6f6d0d3bc9f3ab6c3e26140acdaa8746c59675
Filakcz <blahafila@gmail.com>
2025-10-17 21:45:46 +0200

    sum linked list, fixes

--- C_sharp/LinkedList/Program.cs
+++ C_sharp/LinkedList/Program.cs
@@ -5,11 +5,7 @@
         static void Main(string[] args)
         {
             LinkedList spojak = new LinkedList();
-            spojak.AddToEnd(3);
-            spojak.AddToEnd(4);
-            spojak.AddToEnd(5);
-            spojak.AddToEnd(6);
-            spojak.AddToEnd(4);
+            spojak.AddMultiple(3,4,5,6,4);
 
             Console.Write("Linked list: ");
             spojak.Print();
@@ -28,28 +24,48 @@
             Console.Write("Removing last, new linked list: ");
             spojak.RemoveLast();
             spojak.Print();
-
+            
+            Console.WriteLine();
 
             Console.WriteLine("Is there 5 ; 7?");
             Console.WriteLine(spojak.IsThere(5));
             Console.WriteLine(spojak.IsThere(7));
 
+            Console.WriteLine();
+           
+            Console.Write("List 1: ");
+            spojak.Print();
             Console.Write("List 2: ");
             LinkedList spojak2 = new LinkedList();
-            spojak2.AddToEnd(4);
-            spojak2.AddToEnd(7);
-            spojak2.AddToEnd(1);
-            spojak2.AddToEnd(5);
-            spojak2.AddToEnd(5);
+            spojak2.AddMultiple(4,7,1,5,5);
             spojak2.Print();
 
-            LinkedList intersect = LinkedList.Intersection(spojak, spojak2);
+            // clone protoze prunik je destruktivni a nechce se mi vytvaret novy spojak
+            LinkedList intersect = LinkedList.Intersection(spojak.Clone(), spojak2.Clone());
             Console.Write("Intersection: ");
             intersect.Print();
-            
+                       
+            spojak2.AddMultiple(4,7,1,5,5);
+
             LinkedList union = LinkedList.Union(spojak, spojak2);
             Console.Write("Union: ");
             union.Print();
+
+            Console.WriteLine();
+
+            LinkedList a = new LinkedList();
+            LinkedList b = new LinkedList();
+            a.AddMultiple(4,1,0,2,3,2);
+            b.AddMultiple(0,2,1,8);
+
+            Console.Write("a: ");
+            a.Print();
+            Console.Write("b: ");
+            b.Print();
+            
+            LinkedList sum = LinkedList.AddNumbers(a, b);
+            Console.Write("Sum: ");
+            sum.Print();
         }
     }
 
@@ -68,6 +84,9 @@
     class LinkedList
     {
         public Node? Head { get; set; }
+        
+        // O(n)
+        // lepsi by bylo ulozit koncovy node, coz by bylo O(1)
         public void AddToEnd(int value)
         {
             if(Head == null)
@@ -77,18 +96,29 @@
             else
             {
                 Node currentNode = Head;
-                while(currentNode.Next != null)
+                while (currentNode.Next != null)
                 {
                     currentNode = currentNode.Next;
                 }
                 currentNode.Next = new Node(value);
+            }            
+        }
+        
+        // O(n*m), m = pocet pridanych prvku
+        // opet by slo zlepsit zlepsenim AddToEnd na O(1)
+        public void AddMultiple(params int[] values)
+        {
+            foreach (int value in values)
+            {
+                AddToEnd(value);
             }
         }
-
+         
+        // O(n)
         public void Print()
         {
             Node? node = Head;
-            while ( node!=null)
+            while (node != null)
             {
                 Console.Write(node.Value);
                 if (node.Next != null)
@@ -101,6 +131,7 @@
 
         }
 
+        // O(n)
         public int? Max()
         {
             if (Head == null)
@@ -121,6 +152,8 @@
             return max;
         }
 
+        // O(n)
+        // dalo by se zrychlit pokud bychom meli ulozeno i zpet
         public void RemoveLast()
         {
             if (Head == null)
@@ -141,6 +174,22 @@
             node.Next = null;
         }
 
+        // O(n^2) - kazde AddToEnd je O(n)
+        // zrychlelo by se zlepsenim AddToEnd na O(1)
+        // nastesti nam nezalezi na rychlosti
+        public LinkedList Clone()
+        {
+            LinkedList copy = new LinkedList();
+            Node? current = Head;
+            while (current != null)
+            {
+                copy.AddToEnd(current.Value);
+                current = current.Next;
+            }
+            return copy;
+        }
+
+        // O(n)
         public bool IsThere(int value)
         {
             Node? node = Head;
@@ -156,6 +205,9 @@
             return false;
         } 
 
+        // O(n*m)
+        // pro kazdy node se pouziva IsThere O(m)
+        // ukladani konce resultu at se nemusi prochazet cely
         public static LinkedList Intersection(LinkedList list1, LinkedList list2)
         {
             if (list1.Head == null || list2.Head == null)
@@ -198,6 +250,8 @@
             return result;
         }
 
+        // O(n*m)
+        // stejny jak prunik
         public static LinkedList Union(LinkedList list1, LinkedList list2)
         {
             if (list1.Head == null)
@@ -230,8 +284,71 @@
                 node2 = next;
             }
 
+            list2.Head = null;
             return list1;
         }
- 
+        
+        // O(n)
+        public void Reverse()
+        {
+            Node? previous = null;
+            Node? current = Head;
+
+            while (current != null)
+            {
+                Node? next = current.Next;
+                current.Next = previous;
+                previous = current;
+                current = next;
+            }
+
+            Head = previous;
+        }
+
+        // O(n + m)
+        public static LinkedList AddNumbers(LinkedList a, LinkedList b)
+        {
+            a.Reverse();
+            b.Reverse();
+
+            Node? nodeA = a.Head;
+            Node? nodeB = b.Head;
+            int carry = 0;
+
+            LinkedList result = new LinkedList();
+            Node? last = null;
+
+            while (nodeA != null || nodeB != null || carry > 0)
+            {
+                int sum = carry;
+                if (nodeA != null)
+                {
+                    sum += nodeA.Value;
+                    nodeA = nodeA.Next;
+                }
+
+                if (nodeB != null)
+                {
+                    sum += nodeB.Value;
+                    nodeB = nodeB.Next;
+                }
+
+                Node newNode = new Node(sum % 10);
+                carry = sum / 10;
+                if (result.Head == null)
+                {
+                    result.Head = newNode;
+                    last = newNode;
+                }
+                else
+                {
+                    last!.Next = newNode;
+                    last = newNode;
+                }
+            }
+
+            result.Reverse();
+            return result;
+        }
     }
 }
