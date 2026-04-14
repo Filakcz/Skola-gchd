34879cabae9f1bf404dcd60c1786d36bc7d08c88
Filakcz <blahafila@gmail.com>
2026-04-14 19:37:20 +0200

    BogoSort

---
+++ CSharp/BogoSort/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace BogoSort;
+
+class Program
+{
+    static Random random = new Random();
+
+    static void Main(string[] args)
+    {
+        Console.Write("BogoSort: ");
+        string[] stringArray = Console.ReadLine()!.Split();
+        int[] array = new int[stringArray.Length];
+        for (int i = 0; i < stringArray.Length; i++)
+        {
+            array[i] = Convert.ToInt32(stringArray[i]);
+        }
+
+        BogoSort(array);
+    
+        Console.Write("Sorted: ");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+        Console.WriteLine();
+    }
+
+    static void BogoSort(int[] array)
+    {
+        while (!IsSorted(array))
+        {
+            Shuffle(array);
+        }
+    }
+
+    static bool IsSorted(int[] array)
+    {
+        for (int i = 0; i < (array.Length - 1); i++)
+        {
+            if (array[i] > array[i + 1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void Shuffle(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            int j = random.Next(array.Length);
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
---
+++ CSharp/Kalkulacka/Program.cs
@@ -0,0 +1,99 @@
+﻿namespace Kalkulacka;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("Prefix (r) nebo postfix (o): ");
+            string? action = Console.ReadLine();
+
+            if (action == "r")
+            {
+                Console.Write("Výraz: ");
+                string[] expression = Console.ReadLine()!.Split();
+                Console.Write("> ");
+
+                PreFix(expression);
+            }
+            else if (action == "o")
+            {
+                Console.Write("Výraz: ");
+                string[] expression = Console.ReadLine()!.Split();
+                Console.Write("> ");
+
+                PostFix(expression);
+            }
+            else
+            {
+                Console.WriteLine("Neplatná akce!");
+            }
+        }
+    }
+
+    static void PostFix(string[] expression)
+    {
+        Stack<double> stack = new Stack<double>();
+        foreach (string cur in expression)
+        {
+            if (double.TryParse(cur, out double operand))
+            {
+                stack.Push(operand);
+            }
+            else if (stack.Count > 1)
+            {
+                double right = stack.Pop();
+                double left = stack.Pop();
+
+                if (cur == "+")
+                {
+                    stack.Push(left + right);
+                }
+                else if (cur == "-")
+                {
+                    stack.Push(left - right);
+                }
+                else if (cur == "*")
+                {
+                    stack.Push(left * right);
+                }
+                else if (cur == "/")
+                {
+                    if (right == 0)
+                    {
+                        Console.WriteLine("Dělení nulou není definováno!");
+                        return;
+                    }
+                    stack.Push(left / right);
+                }
+                else
+                {
+                    Console.WriteLine("Neplatný výraz: neplatný operátor/y");
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Neplatný výraz: chybí operand/y");
+                return;
+            }
+
+        }
+
+        if (stack.Count == 1)
+        {
+            Console.WriteLine(stack.Pop());
+        }
+        else
+        {
+            Console.WriteLine("Neplatný výraz: chybí operátor/y");
+        }
+    }
+
+    static void PreFix(string[] expression)
+    {
+        Stack<string> stack = new Stack<string>();
+
+    }
+}
