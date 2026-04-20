8ad8a5141ae8699ea6c09bc8bc9ac75ba670dcb9
Filakcz <blahafila@gmail.com>
2026-04-20 20:27:07 +0200

    Kalkulacka

--- CSharp/Kalkulacka/Program.cs
+++ CSharp/Kalkulacka/Program.cs
@@ -13,7 +13,6 @@ class Program
             {
                 Console.Write("Výraz: ");
                 string[] expression = Console.ReadLine()!.Split();
-                Console.Write("> ");
 
                 PreFix(expression);
             }
@@ -21,7 +20,6 @@ class Program
             {
                 Console.Write("Výraz: ");
                 string[] expression = Console.ReadLine()!.Split();
-                Console.Write("> ");
 
                 PostFix(expression);
             }
@@ -35,43 +33,37 @@ class Program
     static void PostFix(string[] expression)
     {
         Stack<double> stack = new Stack<double>();
+        Stack<string> infixStack = new Stack<string>();
+        bool error = false;
+
         foreach (string cur in expression)
         {
             if (double.TryParse(cur, out double operand))
             {
                 stack.Push(operand);
+                infixStack.Push(cur);
             }
             else if (stack.Count > 1)
             {
                 double right = stack.Pop();
                 double left = stack.Pop();
-
-                if (cur == "+")
+                
+                double ans = DoOperation(cur, left, right);
+                if (double.IsPositiveInfinity(ans))
                 {
-                    stack.Push(left + right);
+                    error = true;
+                    ans = 0;
                 }
-                else if (cur == "-")
+                else if (double.IsNaN(ans))
                 {
-                    stack.Push(left - right);
-                }
-                else if (cur == "*")
-                {
-                    stack.Push(left * right);
-                }
-                else if (cur == "/")
-                {
-                    if (right == 0)
-                    {
-                        Console.WriteLine("Dělení nulou není definováno!");
-                        return;
-                    }
-                    stack.Push(left / right);
-                }
-                else
-                {
-                    Console.WriteLine("Neplatný výraz: neplatný operátor/y");
                     return;
                 }
+
+                stack.Push(ans);
+
+                string rightInfix = infixStack.Pop();
+                string leftInfix = infixStack.Pop();
+                infixStack.Push($"({leftInfix} {cur} {rightInfix})");
             }
             else
             {
@@ -83,7 +75,11 @@ class Program
 
         if (stack.Count == 1)
         {
-            Console.WriteLine(stack.Pop());
+            Console.WriteLine($"Infix: {infixStack.Pop()}");
+            if (!error)
+            {
+                Console.WriteLine($"Výsledek: {stack.Pop()}");
+            }
         }
         else
         {
@@ -94,6 +90,100 @@ class Program
     static void PreFix(string[] expression)
     {
         Stack<string> stack = new Stack<string>();
+        Stack<string> infixStack = new Stack<string>();
+        bool error = false;
+        int operandCount = 0;
+        int operatorCount = 0;
 
+
+        foreach (string cur in expression)
+        {
+            if (double.TryParse(cur, out double num))
+            {
+                operandCount++;
+                double current = num;
+                string currentInfix = cur;
+
+                while (stack.Count() > 1 && double.TryParse(stack.Peek(), out double stackNum))
+                {
+                    stack.Pop();
+                    string op = stack.Pop();
+                    string stackInfix = infixStack.Pop();
+    
+                    double ans = DoOperation(op, stackNum, current);
+
+                    if (double.IsPositiveInfinity(ans))
+                    {
+                        error = true;
+                        ans = 0;
+                    }
+                    else if (double.IsNaN(ans))
+                    {
+                        return;
+                    }
+                   
+                    current = ans;
+                    currentInfix = $"({stackInfix} {op} {currentInfix})";
+                }
+
+                stack.Push(Convert.ToString(current));
+                infixStack.Push(currentInfix);
+            }
+            else
+            {
+                operatorCount++;
+                stack.Push(cur);
+            }
+        }
+
+        if (stack.Count == 1)
+        {
+            Console.WriteLine($"Infix: {infixStack.Pop()}");
+            if (!error)
+            {
+                Console.WriteLine($"Výsledek: {stack.Pop()}");
+            }
+
+        }
+        else if (operandCount > operatorCount + 1)
+        {
+            Console.WriteLine("Neplatný výraz: chybí operátor/y");
+        }
+        else
+        {
+            Console.WriteLine("Neplatný výraz: chybí operand/y");
+        }
+
+    }
+
+    static double DoOperation(string op, double left, double right)
+    {
+        // switch case by byl lepsi
+        if (op == "+")
+        {
+            return (left + right);
+        }
+        else if (op == "-")
+        {
+            return (left - right);
+        }
+        else if (op == "*")
+        {
+            return (left * right);
+        }
+        else if (op == "/")
+        {
+            if (right == 0)
+            {
+                Console.WriteLine("Dělení nulou není definováno!");
+                return double.PositiveInfinity;
+            }
+            return (left / right);
+        }
+        else
+        {
+            Console.WriteLine("Neznámý operátor");
+            return double.NaN;
+        }
     }
 }
